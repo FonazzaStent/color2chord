02e95494ada452f2d07448d86ff0dd1a6cd21635
Maurizio Crescenzo <92685677+FonazzaStent@users.noreply.github.com>
2023-03-31 11:38:41 +0200

    Add files via upload

---
+++ ReadMe.txt
@@ -0,0 +1,8 @@
+Color to Chord 1.1.0
+
+The program will convert colors in RGB format to 5 note chords. Run the executable and you will be prompted
+for R, G, and B value of your color. Input the values and hit enter to generate the chord. The chord will
+be displayed in letter notation format. Hit Enter for a random value. You can set random values for each
+R, G and B channel individually and independently from the other two channels, so the color can have one,
+two or all three channels set on a random value.
+Input more R, G, B values for another chord or "q" to quit the program.
---
+++ color2chord_1.0.1.py
@@ -0,0 +1,120 @@
+"""Color to Chord 1.0.1 - Convert colors to chords.
+Copyright (C) 2023  Fonazza-Stent
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>."""
+
+import random
+from random import randint
+
+notes=["C","C#","D","D#","E","F","F#","G","G#","A","A#","B"]
+rgb_scale = 255
+cmyk_scale = 100
+
+def inputRGB():
+    global R
+    global G
+    global B
+    global quitcheck
+    print ("Input R, G, B values for a color in RGB format."+"\n")
+    R=input("R (q to quit, Enter for random value): ")
+    if R=='':
+        R=str(randint(1,255))
+        print (R)
+    if R=='q':
+        quit()
+    if R.isdigit()==False or int(R)<0:
+        R=0
+    if int(R)>255:
+        R=255
+    G=input("G (q to quit, Enter for random value): ")
+    if G=='':
+        G=str(randint(1,255))
+        print (G)
+    if G=='q':
+        quit()
+    if G.isdigit()==False or int(G)<0:
+        G=0
+    if int(G)>255:
+        G=255
+    B=input("B (q to quit, Enter for random value): ")
+    if B=='':
+        B=str(randint(1,255))
+        print (B)
+    if B=='q':
+        quit()
+    if B.isdigit()==False or int(B)<0:
+        B=0
+    if int(B)>255:
+        B=255
+    R=int(R)
+    G=int(G)
+    B=int(B)
+    GenerateChord(R,G,B)
+    
+
+def rgb_to_cmyk(r,g,b):
+    if (r == 0) and (g == 0) and (b == 0):
+        # black
+        return 0, 0, 0, cmyk_scale
+
+    # rgb [0,255] -> cmy [0,1]
+    c = 1 - r / float(rgb_scale)
+    m = 1 - g / float(rgb_scale)
+    y = 1 - b / float(rgb_scale)
+
+    # extract out k [0,1]
+    min_cmy = min(c, m, y)
+    c = (c - min_cmy) 
+    m = (m - min_cmy) 
+    y = (y - min_cmy) 
+    k = min_cmy
+
+    # rescale to the range [0,cmyk_scale]
+    cmyk=[ int(c*cmyk_scale), int(m*cmyk_scale), int(y*cmyk_scale), int(k*cmyk_scale)]
+    return cmyk
+
+#GenerateChord
+
+def GenerateChord(R,G,B):
+    cmyk=rgb_to_cmyk(R,G,B)
+    chord=[]
+    rootvalue=int(((R+G+B)/3)/21.25)
+    root=notes[rootvalue]
+    chord.append(root)
+    notevalue=rootvalue
+    #print (notevalue,root)
+        
+    for value in cmyk:
+        try:
+            interval=int(value/16.66)+3
+        except:
+            interval=3
+        notevalue=notevalue+interval
+        if notevalue>11:
+            notevalue=notevalue-11
+        chord.append(notes[notevalue])
+    chorddisplay=''
+    for item in chord:
+        chorddisplay= chorddisplay+item+" "
+    print ("\n"+"Chord: "+chorddisplay+"\n")
+
+#main
+def main():
+    global R
+    global G
+    global B
+    while True:
+        inputRGB()
+
+main()
